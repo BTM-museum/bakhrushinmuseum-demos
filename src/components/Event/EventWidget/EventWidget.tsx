74c27612299c5a6d017e20e7e7c6fb1b85c0e17b
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2024-04-10 08:50:45 +0300

    init

---
+++ src/components/Event/EventWidget/EventWidget.tsx
@@ -0,0 +1,7 @@
+import styles from './EventWidget.module.scss';
+
+const EventWidget = () => {
+    return <></>
+};
+
+export default EventWidget;
--- src/pages/ExcursionPage/ExcursionPage.tsx
+++ src/pages/ExcursionPage/ExcursionPage.tsx
@@ -1,10 +1,24 @@
 import styles from './ExcursionPage.module.scss';
 import {useLocation} from "react-router-dom";
+import {transliterate} from "../../helpers/translate";
+import {useMemo} from "react";
+import {useRecoilValue} from "recoil";
+import {excursions} from "../../store/excursions/excursions";
 
 const ExcursionPage = () => {
     const { pathname } = useLocation();
+    const excursion = useRecoilValue(excursions);
+
+    const header = transliterate(pathname.split('/')[2], true);
+    const content = excursion.find((item) => item.title === header);
     return <div className={styles.container}>
-        <h1>{`ExcursionPage number: ${pathname}`}</h1>
+        <h1>
+            {`ExcursionPage number: ${pathname}`}
+            {
+                content ? content.page : 'Not Found'
+            }
+
+        </h1>
     </div>
 };
 
