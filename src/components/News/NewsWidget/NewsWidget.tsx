4f59f80d6083b39afcafc1e0b99311d59e0cc1d0
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2024-04-24 11:55:48 +0300

    init

--- src/components/News/NewsWidget/NewsWidget.tsx
+++ src/components/News/NewsWidget/NewsWidget.tsx
@@ -13,7 +13,7 @@ const NewsWidget = () => {
             initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -30 }}
-            viewport={{ once: false, amount: 0.2 }}
+            viewport={{ once: false, amount: 0.1 }}
             transition={{ duration: 0.3, type: 'ease' }}
 
             className={styles.header}>
